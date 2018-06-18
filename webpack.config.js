4bf6b507b76d0f060b90142bc4dc9bac723e88d8
kyle <kyle@solidstategroup.com>
2018-06-18 16:53:19 +0100

    Simplify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -10,62 +10,63 @@ const defaultConfig = {
                 use: ['babel-loader']
             }
         ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
     }
 };
 
+const webBundle = Object.assign({}, defaultConfig, { //Bundle 1: compile the web client
+    output: {
+        filename: "index.js",
+        library: "bullet-train",
+        libraryTarget: "umd",
+        path: path.join(__dirname, '/bullet-train-client/lib'),
+    },
+    entry: {
+        main: './index.js'
+    }
+});
+
+const webExampleBundle = Object.assign({}, defaultConfig, {  //Bundle 2: compile the web client for the example project
+    output: {
+        filename: "bullet-train.js",
+        library: "bullet-train",
+        libraryTarget: "umd",
+        path: path.join(__dirname, '/bullet-train-client/example/src'),
+    },
+    entry: {
+        main: './index.js'
+    }
+});
+
+const reactNativeBundle = Object.assign({}, defaultConfig, { //Bundle 3: compile the react native client
+    entry: {
+        main: './index.react-native.js'
+    },
+    externals: {
+        'react-native': 'react-native'
+    },
+    output: {
+        filename: "bullet-train.js",
+        library: "bullet-train",
+        libraryTarget: "umd",
+        path: path.join(__dirname, '/react-native-bullet-train/example'),
+    }
+});
+
+const reactNativeExampleBundle = Object.assign({}, defaultConfig, { //Bundle 4: compile the react native client for the example project
+    entry: {
+        main: './index.react-native.js'
+    },
+    externals: {
+        'react-native': 'react-native'
+    },
+    output: {
+        filename: "index.js",
+        library: "bullet-train",
+        libraryTarget: "umd",
+        path: path.join(__dirname, '/react-native-bullet-train/lib'),
+    }
+});
 
 module.exports = [
-    Object.assign({}, defaultConfig, { //compile the example
-        output: {
-            filename: "index.js",
-            library: "bullet-train",
-            libraryTarget: "umd",
-            path: path.join(__dirname, '/bullet-train-client/lib'),
-        },
-        entry: {
-            main: './index.js'
-        }
-    }),
-    Object.assign({}, defaultConfig, { //compile the example
-        output: {
-            filename: "bullet-train.js",
-            library: "bullet-train",
-            libraryTarget: "umd",
-            path: path.join(__dirname, '/bullet-train-client/example/src'),
-        },
-        entry: {
-            main: './index.js'
-        }
-    }),
-    Object.assign({}, defaultConfig, { //compile the example
-        entry: {
-            main: './index.react-native.js'
-        },
-        externals: {
-            'react-native' : 'react-native' // Case matters here
-        },
-        output: {
-            filename: "bullet-train.js",
-            library: "bullet-train",
-            libraryTarget: "umd",
-            path: path.join(__dirname, '/react-native-bullet-train/example'),
-        }
-    }),
-    Object.assign({}, defaultConfig, { //compile the example
-        entry: {
-            main: './index.react-native.js'
-        },
-        externals: {
-            'react-native' : 'react-native' // Case matters here
-        },
-        output: {
-            filename: "index.js",
-            library: "bullet-train",
-            libraryTarget: "umd",
-            path: path.join(__dirname, '/react-native-bullet-train/lib'),
-        }
-    }),
+    webBundle, webExampleBundle, reactNativeBundle, reactNativeExampleBundle,
 ];
