21f6df1a92a5a8d873ec3155e3f68897522f4432
kyle <kyle@solidstategroup.com>
2019-10-27 17:15:26 +0000

    Replace window with global

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import AsyncStorage from "@callstack/async-storage";
 require('whatwg-fetch');
 const bt = require('./bullet-train-core');
-const bulletTrain = bt({AsyncStorage, fetch: window.fetch});
-window.bulletTrain = bulletTrain;
+const bulletTrain = bt({AsyncStorage, fetch: global.fetch});
+global.bulletTrain = bulletTrain;
 module.exports = bulletTrain;
