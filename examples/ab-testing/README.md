6f0d75752c04c01ad3322e7967917ca5c304d39e
kyle <kyle@solidstategroup.com>
2019-11-18 21:02:02 +0000

    Add ab test example

---
+++ examples/ab-testing/README.md
@@ -0,0 +1,30 @@
+<img width="100%" src="https://raw.githubusercontent.com/SolidStateGroup/bullet-train-frontend/master/hero.png"/>
+
+## Bullet Train A/B Testing
+<img src="./example.png"/>
+This repository contains an example of an a/b test.
+
+## Installation 
+None! Just open index.html in your preferred browser.
+
+## How was this feature created in Bullet Train?
+
+**Step 1: Create a remote config called hero**
+
+<img src="./step1.png"/>
+
+Set the value to be the image path you want
+
+**Step 2: Create a segment with a percentage split**
+
+<img src="./step2.png"/>
+
+This will mean you can override features for 50% of your users.
+
+**Step 3: Add another segment or user override**
+
+<img src="./step3.png"/>
+
+You can always override your % split override for particular users or other segments.
+
+
---
+++ examples/ab-testing/main.js
@@ -0,0 +1,29 @@
+var environmentID = 'QjgYur4LQTwe5HpvbvhpzK'
+
+$(document).ready(function () {
+    //Intialise Bullet Train
+    $('.js-special').on('click', function () {
+        bulletTrain.identify('bullet_train_sample_user')
+    });
+    $('.js-random').on('click', function () {
+        bulletTrain.identify("random_" + Math.floor(Math.random() * 10))
+    });
+    bulletTrain.init({
+        environmentID: environmentID,
+        defaultFlags: {
+            font_size: 10
+        },
+        onChange: function () {
+            if (bulletTrain.getValue('hero')) {
+                $('#loaded').removeClass('hidden')
+                $('#loading').addClass('hidden')
+                $('.hero').css({
+                    'background': 'url(' + bulletTrain.getValue('hero') + ')',
+                    'background-repeat': 'no-repeat',
+                    'background-size': 'cover'
+                })
+            }
+        }
+    });
+
+})
