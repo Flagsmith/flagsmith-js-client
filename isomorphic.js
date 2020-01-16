008bc4fb59303633e0ea48646c6487673426bc2a
kyle <kyle@solidstategroup.com>
2020-01-16 10:33:40 +0000

    re-add isomorphic.js

---
+++ isomorphic.js
@@ -0,0 +1,12 @@
+let fetch = require("unfetch").default;
+if (typeof window === 'undefined')  {
+    fetch  = require('node-fetch').default;
+}
+
+import AsyncStorage from "@callstack/async-storage";
+const bt = require('./bullet-train-core');
+const bulletTrain = bt({AsyncStorage, fetch});
+if (typeof window !== "undefined") {
+    window.bulletTrain = bulletTrain;
+}
+export default bulletTrain;
