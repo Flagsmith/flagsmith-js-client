953134c52583cea65f7ccbaff0981df5a868d523
kyle-ssg <kyle@solidstategroup.com>
2021-06-15 12:04:33 +0100

    react native hotfix

--- index.react-native.js
+++ index.react-native.js
@@ -3,5 +3,5 @@ import AsyncStorage from "@callstack/async-storage/lib";
 const core = require('./flagsmith-core');
 export default core({});
 export const createFlagsmithInstance = ()=>{
-    export default core({});
+    return core({})
 }
