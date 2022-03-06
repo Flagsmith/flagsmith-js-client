25e5a7376325f7531bb6943d0745e03a7fb3b71c
kyle-ssg <kyle@solidstategroup.com>
2022-03-06 15:19:09 +0000

    add traits

--- isomorphic.ts
+++ isomorphic.ts
@@ -1,11 +1,6 @@
-let fetch = require("unfetch").default;
-if (typeof window === 'undefined')  {
-    fetch  = require('node-fetch').default;
-}
-
 import AsyncStorage from "@callstack/async-storage";
 const core = require('./flagsmith-core');
-const flagsmith = core({AsyncStorage, fetch});
+const flagsmith = core({AsyncStorage});
 if (typeof window !== "undefined") {
     // @ts-ignore
     window.flagsmith = flagsmith;
