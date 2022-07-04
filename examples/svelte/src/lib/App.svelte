02db1d14626889bb62379cda4677093f7a53ca5b
kyle-ssg <kyle@solidstategroup.com>
2022-07-04 10:25:29 +0100

    Update svelte example to show identities

--- examples/svelte/src/lib/App.svelte
+++ examples/svelte/src/lib/App.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import { browser } from '$app/env';
+
+	const updateFeatures = ()=> {
+		fontSize = flagsmith.getValue("font_size");
+		loggedIn = !!flagsmith.identity;
+	}
 
 	import flagsmith from "flagsmith/isomorphic"
 	export let flagsmithState;
@@ -8,14 +12,44 @@
 		console.log("Initialising clientside Flagsmith with server state", flagsmithState)
 		flagsmith.init({
 			environmentID: "QjgYur4LQTwe5HpvbvhpzK",
-			state: flagsmithState
+			state: flagsmithState,
 		})
 	}
+
+	let loggedIn = !!flagsmith.identity;
+	let fontSize = flagsmith.getValue("font_size");
+
+	function login () {
+		flagsmith.identify("flagsmith_sample_user")
+			.then(updateFeatures)
+	};
+
+	function logout () {
+		flagsmith.logout().then(updateFeatures)
+	};
 </script>
 
 <div class="counter">
-	Font Size: <strong class="hidden" aria-hidden="true">{
-		flagsmith.getValue("font_size")
+	<div>
+		Font Size: <strong class="hidden" aria-hidden="true">{
+		fontSize
 	}</strong>
+	</div>
+	{#if !loggedIn}
+		<button on:click={login}>
+			Log in
+		</button>
+	{/if}
+	{#if loggedIn}
+		<div>
+			Example Trait: <strong className='hidden' aria-hidden='true'>{
+			flagsmith.getTrait("example_trait")
+		}</strong>
+		</div>
+		<button on:click={logout}>
+			Logout
+		</button>
+	{/if}
+
 </div>
 
