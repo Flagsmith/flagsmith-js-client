092ee50f3f02589f2f8f76acccb42341bf093907
kyle-ssg <kyle@solidstategroup.com>
2022-07-15 12:00:37 +0100

    Add deno-fresh example

---
+++ examples/deno-fresh/routes/index.tsx
@@ -0,0 +1,28 @@
+/** @jsx h */
+import { h } from "preact";
+import flagsmith from 'flagsmith'
+import { Handlers, PageProps } from "$fresh/server.ts";
+const environmentID = "QjgYur4LQTwe5HpvbvhpzK"
+
+type Props = {
+    font_size: number
+}
+export const handler: Handlers<Props> = {
+    async GET(_req, ctx) {
+        console.log(flagsmith)
+
+        await flagsmith.init({ // fetches flags on the server
+            environmentID,
+        });
+        return ctx.render({font_size:flagsmith.getValue("font_size")});
+    },
+};
+
+
+export default function Page({ data }: PageProps<Props | null>) {
+  return (
+    <div>
+        {data.font_size}
+    </div>
+  );
+}
