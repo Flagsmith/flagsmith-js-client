4123df0f9491a0a6a0e99466b601309034a46745
kyle-ssg <kyle@solidstategroup.com>
2022-08-05 11:11:32 +0100

    update flagsmith plugin

--- examples/nuxt/plugins/flagsmith-plugin.ts
+++ examples/nuxt/plugins/flagsmith-plugin.ts
@@ -3,7 +3,6 @@ import flagsmith from 'flagsmith'
 const plugin: Plugin = async ({beforeNuxtRender, app, nuxtState}, inject) => {
   if (process.server) {
     beforeNuxtRender(async ({nuxtState}) => {
-      console.log("fetch is", fetch)
       const nodeFetch = require('node-fetch').default
       await flagsmith.init({
         environmentID: 'QjgYur4LQTwe5HpvbvhpzK',
@@ -11,19 +10,11 @@ const plugin: Plugin = async ({beforeNuxtRender, app, nuxtState}, inject) => {
         enableLogs: true,
         api: 'https://api.flagsmith.com/api/v1/'
       })
-      try {
-        await flagsmith.getFlags()
-
-      } catch(e) {
-        console.log(e)
-      }
-      console.log(flagsmith.getState())
       nuxtState.flagsmith = flagsmith.getState()
     })
   }
 
   if (process.client) {
-    console.log('hi')
     if (nuxtState.flagsmith) {
 // @ts-expect-error
       flagsmith.setState(nuxtState.flagsmith)
