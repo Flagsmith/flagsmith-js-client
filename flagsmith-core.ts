8f87dd28a8848d75ddf0b9954b56747ae12beaf3
Aey-Thidarat <thidarat.c@pomelofashion.com>
2023-02-02 13:38:33 +0700

    revert change

--- flagsmith-core.ts
+++ flagsmith-core.ts
@@ -171,8 +171,7 @@ const Flagsmith = class {
                             "trait_key":k,
                             "trait_value": this.withTraits![k]
                         }))
-                    }))
-                    :this.getJSON(api + 'identities/?identifier=' + encodeURIComponent(identity)),
+                    })):this.getJSON(api + 'identities/?identifier=' + encodeURIComponent(identity)),
             ])
                 .then((res) => {
                     this.withTraits = null
