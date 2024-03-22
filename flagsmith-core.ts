9d25ca426e6884ea98acd9ba4d369dc99a674d3f
kyle-ssg <kyle@solidstategroup.com>
2024-03-22 20:23:11 +0000

    Reverse traits

--- flagsmith-core.ts
+++ flagsmith-core.ts
@@ -719,7 +719,8 @@ const Flagsmith = class {
             this.identity = state.identity || this.identity;
             this.traits = state.traits || this.traits;
             this.withTraits = {
-               ...this.traits
+                ...(this.withTraits||{}),
+                ...this.traits,
             };
             this.evaluationEvent = state.evaluationEvent || this.evaluationEvent;
             this.log("setState called", this)
