2daf0a716a4f09b7679aaf29982bc229a8002227
kyle-ssg <kyle@solidstategroup.com>
2024-04-05 11:19:36 +0100

    Add changed tests

--- test/default-flags.test.ts
+++ test/default-flags.test.ts
@@ -1,5 +1,6 @@
 // Sample test
 import { defaultState, defaultStateAlt, getFlagsmith, getStateToCheck } from './test-constants';
+import { IFlags } from '../types';
 
 describe('Default Flags', () => {
 
@@ -9,7 +10,7 @@ describe('Default Flags', () => {
     test('should error and not hit the API when preventFetch is true without default flags', async () => {
         const onChange = jest.fn();
         const { flagsmith, initConfig, AsyncStorage, mockFetch } = getFlagsmith({ onChange, preventFetch: true });
-        await flagsmith.init(initConfig);
+        await flagsmith.init(initConfig)
 
         expect(AsyncStorage.getItem).toHaveBeenCalledTimes(1);
         expect(mockFetch).toHaveBeenCalledTimes(0);
@@ -28,6 +29,62 @@ describe('Default Flags', () => {
             ...defaultState,
             flags: {},
         });
+    });
+    test.only('should return accurate changed flags', async () => {
+        const onChange = jest.fn();
+        const defaultFlags: IFlags = {
+            string_value:{id:1,enabled:true,value:"test"},
+            numeric_value:{id:2,enabled:true,value:1},
+            boolean_value:{id:3,enabled:true,value:true},
+            unchanged_string_value:{id:4,enabled:true,value:"test"},
+            unchanged_numeric_value:{id:5,enabled:true,value:1},
+            unchanged_boolean_value:{id:6,enabled:true,value:null},
+        }
+        const itemsToRemove:IFlags = {
+            string_value_remove:{id:7,enabled:true,value:"test"},
+            numeric_value_remove:{id:8,enabled:true,value:1},
+            boolean_value_remove:{id:9,enabled:true,value:true},
+        }
+        const { flagsmith, initConfig, AsyncStorage, mockFetch } = getFlagsmith({
+            onChange,
+            preventFetch: true,
+            cacheFlags: true,
+            defaultFlags: {...defaultFlags, ...itemsToRemove},
+        });
+        await AsyncStorage.setItem('BULLET_TRAIN_DB', JSON.stringify({
+            ...defaultState,
+            flags: {
+                ...defaultFlags,
+                string_value:{...defaultFlags.string_value, value:"test2"},
+                numeric_value:{...defaultFlags.numeric_value,value:2},
+                boolean_value:{...defaultFlags.boolean_value,enabled:false},
+                new_string_value:{...defaultFlags.string_value},
+                new_numeric_value:{...defaultFlags.numeric_value},
+                new_boolean_value:{...defaultFlags.boolean_value},
+            }
+        }));
+        await flagsmith.init(initConfig);
+
+        expect(AsyncStorage.getItem).toHaveBeenCalledTimes(2);
+        expect(mockFetch).toHaveBeenCalledTimes(0);
+        expect(onChange).toHaveBeenCalledTimes(1);
+        expect(onChange).toHaveBeenCalledWith(
+            null,
+            { 'flagsChanged': expect.arrayContaining([
+                "string_value",
+                "numeric_value",
+                "boolean_value",
+                "new_string_value",
+                "new_numeric_value",
+                "new_boolean_value",
+                ].concat(Object.keys(itemsToRemove))), 'isFromServer': false, 'traitsChanged': null },
+            {
+                'error': null,
+                'isFetching': false,
+                'isLoading': false,
+                'source': 'CACHE',
+            },
+        );
     });
     test('should call onChange with default flags', async () => {
         const onChange = jest.fn();
